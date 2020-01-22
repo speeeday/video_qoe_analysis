f80d6a32585d869ed3d71daf2a8e7e3e09d6b16f
Sanjay C <sj@system76-pc.localdomain>
2020-01-22 10:53:03 -0800

    updated directories, working on data generation

--- trace_generation/start_mininet.py
+++ trace_generation/start_mininet.py
@@ -35,6 +35,9 @@ parser.add_argument('--num-hosts', help='Number of hosts to connect to switch',
 parser.add_argument('--pcap-name', help='Name of PCAP file to save',
                     type=str, action="store", default='pcaps/s2-eth2.pcapng')
 
+parser.add_argument('--link', help='Name of video link to use',
+                    type=str, action="store", default='https://www.youtube.com/watch?v=NKaA0IPcD_Q')
+
 args = parser.parse_args()
 
 class SingleSwitchTopo(Topo):
@@ -76,7 +79,7 @@ def main():
 
 
 #    raw_input()
-    os.system('sudo dumpcap -i s2-eth2 -w {} &'.format(outfile))
+    os.system('sudo dumpcap -i s1-eth1 -w {} &'.format(outfile))
     sleep(3)
     
     ### EXPERIMENT CODE START ###
@@ -85,11 +88,29 @@ def main():
     
     client = net.get(topo.host_objects[0])
 
-    video_url = 'https://www.youtube.com/watch?v=ZQeSeYNEwHM'
+    video_url = args.link
+
+    ssl_key_dir = '/home/sj/research/video_qoe_analysis/trace_generation/ssl_keys/'
     
-    client.cmd('chromium-browser --no-sandbox --ignore-certificate-errors --user-data-dir=/tmp/nonexistent$(date +%s%N) {} &'.format(video_url))
-    
-    
+#    client.cmd('export SSLKEYLOGFILE=' + ssl_key_dir + args.pcap_name.split('/')[1].split('.')[0] + '-ssl_key.log')
+
+#    client.cmd('su - sj')
+#    client.cmd('firefox {} &'.format(video_url))
+#    client.cmd('chromium-browser --no-sandbox --ignore-certificate-errors --user-data-dir=/tmp/nonexistent$(date +%s%N) {} &'.format(video_url))    
+
+   # current experiment:
+   # SSLKEYLOGFILE is in .bashrc
+   # sudo - sj
+   # firefox <url>
+   # enable HAR (Developer Tools > Web Console > Persist Logs + Clear)
+   # play video
+   # close window once video is finished
+   # rename ssl key based on pcap name
+
+
+   # open a window on h1 to run experiment manually (until sslkeylogfile can be automated)
+    client.cmd('xterm')
+   
     ### EXPERIMENT CODE END   ###
     
     print "Done !"
